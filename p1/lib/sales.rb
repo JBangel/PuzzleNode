8fd5ef70b72661cecc7b2300e8c6d781fd9615ee
Joshua Bangel <falornan@gmail.com>
2014-03-05 16:25:11 -0600

    Corrected rounding location for problem domain

--- p1/lib/sales.rb
+++ p1/lib/sales.rb
@@ -12,6 +12,7 @@ class Sales
     total = transactions
       .select { |t| t[:sku] == sku }
       .map { |t| t[:cost] * rates[t[:currency]] }
+      .map { |t| BigDecimal.new(t, 0).round(2, :banker) }
       .reduce(&:+)
 
     BigDecimal.new(total, 0).round(2, :banker).to_f
